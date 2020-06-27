122b28a5211b2901d4698b0552a3be3a1144c4c5
Jaqueline <jaquelinesv90@gmail.com>
2020-06-27 00:05:14 -0300

    new classes

--- src/com/example/enumJava/DaysOfWeekEnum.java
+++ src/com/example/enumJava/DaysOfWeekEnum.java
@@ -1,5 +1,7 @@
 package com.example.enumJava;
 
 public enum DaysOfWeekEnum {
+	
+	SEGUNDA, TERCA,QUARTA,QUINTA,SEXTA,SABADO,DOMINGO;
 
 }
--- src/com/example/enumJava/Test.java
+++ src/com/example/enumJava/Test.java
@@ -2,8 +2,13 @@ package com.example.enumJava;
 
 public class Test {
 	
+	// foram criadas duas classes uma de constantes e outra de enum
+	// ambas fazem a mesma coisa mas de formas diferentes
+	
+	
 	public static void main(String[] args) {
 		constantUsing();
+		EnumUsing();
 	}
 	
 	private static void constantUsing() {
@@ -16,18 +21,18 @@ public class Test {
 		int domingo = DaysOfWeekConstants.DOMINGO;
 		
 		System.out.println("teste utilizando constantes no Java");
-		printDaysOfWeek(DaysOfWeekConstants.SEGUNDA);
-		printDaysOfWeek(terca);
-		printDaysOfWeek(quarta);
-		printDaysOfWeek(quinta);
-		printDaysOfWeek(sexta);
-		printDaysOfWeek(sabado);
-		printDaysOfWeek(domingo);
+		printDaysOfWeekConstant(DaysOfWeekConstants.SEGUNDA);
+		printDaysOfWeekConstant(terca);
+		printDaysOfWeekConstant(quarta);
+		printDaysOfWeekConstant(quinta);
+		printDaysOfWeekConstant(sexta);
+		printDaysOfWeekConstant(sabado);
+		printDaysOfWeekConstant(domingo);
 		
 	}
 	
 	
-	private static void printDaysOfWeek(int day) {
+	private static void printDaysOfWeekConstant(int day) {
 		switch (day) {
 		case 1:
 			System.out.println("Segunda-feira");
@@ -48,6 +53,51 @@ public class Test {
 			System.out.println("sabado");
 			break;
 		case 7:
+			System.out.println("domingo");
+			break;
+		}
+	}
+	
+	private static void EnumUsing() {
+		DaysOfWeekEnum segunda = DaysOfWeekEnum.SEGUNDA;
+		DaysOfWeekEnum terca = DaysOfWeekEnum.TERCA;
+		DaysOfWeekEnum quarta = DaysOfWeekEnum.QUARTA;
+		DaysOfWeekEnum quinta = DaysOfWeekEnum.QUINTA;
+		DaysOfWeekEnum sexta = DaysOfWeekEnum.SEXTA;
+		DaysOfWeekEnum sabado = DaysOfWeekEnum.SABADO;
+		DaysOfWeekEnum domingo = DaysOfWeekEnum.DOMINGO;
+		
+		System.out.println("Teste utilizando Enum");
+		printDaysOfWeekEnum(DaysOfWeekEnum.SEGUNDA);
+		printDaysOfWeekEnum(terca);
+		printDaysOfWeekEnum(quarta);
+		printDaysOfWeekEnum(quinta);
+		printDaysOfWeekEnum(sexta);
+		printDaysOfWeekEnum(sabado);
+		printDaysOfWeekEnum(domingo);
+	}
+	
+	private static void printDaysOfWeekEnum(DaysOfWeekEnum day) {
+		switch (day) {
+		case SEGUNDA:
+			System.out.println("Segunda-feira");
+			break;
+		case TERCA:
+			System.out.println("terca-feira");
+			break;
+		case QUARTA:
+			System.out.println("quarta-feira");
+			break;
+		case QUINTA:
+			System.out.println("quinta-feira");
+			break;
+		case SEXTA:
+			System.out.println("sexta-feira");
+			break;
+		case SABADO:
+			System.out.println("sabado");
+			break;
+		case DOMINGO:
 			System.out.println("domingo");
 			break;
 		}
