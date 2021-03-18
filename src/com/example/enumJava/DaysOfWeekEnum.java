484efed3e130f0025a90920e41ff91f0b94af491
Jaqueline <jaquelinesv90@gmail.com>
2021-03-18 15:58:04 -0300

    added comments

--- src/com/example/enumJava/DaysOfWeekEnum.java
+++ src/com/example/enumJava/DaysOfWeekEnum.java
@@ -1,5 +1,13 @@
 package com.example.enumJava;
 
+/* A vantagem de utilizar enum sobre constantes é principalmente na hora que a
+* gente ta declarando atributos na classe.
+* ex: eu tenho controle sobre o valor que eu posso colocar nesse atributo, que os valores que eu posso 
+* adicionar a ele  somente podem ser os valores declarados dentro do enum. 
+* ENUM:  DiaSemana segunda = DiaSemana.SEGUNDA;
+* CONSTANTE: int segunda = DiaSemanaConstante.SEGUNDA;(poderia atribuir o valor 10 a esse atributo)
+*/
+
 public enum DaysOfWeekEnum {
 	
 	SEGUNDA, TERCA,QUARTA,QUINTA,SEXTA,SABADO,DOMINGO;
